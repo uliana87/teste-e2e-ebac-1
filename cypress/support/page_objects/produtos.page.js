92bd3ed89e9f3c510ef701db21b5b91304a561e4
uliana arruda <uliana.paiva@hotmail.com>
2025-01-29 17:55:11 -0300

    exercicio e2e

---
+++ cypress/support/page_objects/produtos.page.js
@@ -0,0 +1,18 @@
+class ProdutosPage {
+
+
+    buscarProduto(nomeProduto) {
+        cy.get('[name="s"]').eq(1).type(nomeProduto)
+        cy.get('.button-search ').eq(1).click()
+    }
+
+    addProdutoCarrinho(tamanho, cor, quantidade) {
+        cy.get('.button-variable-item-' + tamanho).click()
+        cy.get(`.button-variable-item-${cor}`).click()
+        cy.wait(1000)
+        cy.get('.input-text').clear().type(quantidade)
+        cy.get('.single_add_to_cart_button').click()
+    }
+}
+
+export default new ProdutosPage() 
